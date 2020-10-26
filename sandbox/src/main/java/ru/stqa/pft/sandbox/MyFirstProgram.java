d1908fd0b18171c8a1740bdeec691e2a5a8dca2d
Lilia3003 <73375560+Lilia3003@users.noreply.github.com>
2020-10-26 21:52:55 +0700

    Задание №2 (добавлены новые функции, классы, объекты и методы)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,13 +3,18 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
     hello();
+Square s = new Square(5);
 
-    double len = 5;
-    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+Rectangle r = new Rectangle(4,5);
 
-    double a = 4;
-    double b = 5;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+Point p1 = new Point(1,1);
+
+Point p2 = new Point(1, 1);
+
+    System.out.println("Расстояние между точками p1(" + p1.x + ";" + p1.y + ") и p2 (" + p2.x + ";" + p2.y +") равно " + p1.distance(p1, p2));
 
   }
 
@@ -18,11 +23,4 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody);
   }
 
-  public static double area(double l) {
-    return l * l;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
 }
