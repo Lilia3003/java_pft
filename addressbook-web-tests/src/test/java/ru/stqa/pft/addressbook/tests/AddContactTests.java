fe93a3b3a26783ba7946fd0ddce525dac65d8e50
Lilia3003 <73375560+Lilia3003@users.noreply.github.com>
2020-11-04 22:26:07 +0700

    Создан класс ApplicationManager

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class AddContactTests extends TestBase {
+
+
+  @Test
+  public void testAddContact() throws Exception {
+
+    app.gotoAddNewContactPage();
+    app.fillContactForm(new ContactData("Name", "LastName", "address", "22-22-22", "test@mail.ru"));
+    app.submitContactCreation();
+    app.returnToHomePage();
+  }
+
+}
