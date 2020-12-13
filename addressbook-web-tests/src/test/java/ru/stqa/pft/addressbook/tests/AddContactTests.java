906fccb122fcb1aeec75d7bd43319ec95fff673d
Lilia3003 <73375560+Lilia3003@users.noreply.github.com>
2020-12-13 12:07:35 +0700

    Применено лямбда-выражение для поиска максимального идентификатора при создании контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
@@ -13,18 +13,12 @@ public class AddContactTests extends TestBase {
   @Test
   public void testAddContact() throws Exception {
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("Name", "LastName", "address", "22-22-22", "test@mail.ru", "1");
+    ContactData contact = new ContactData("NameХ", "LastNameХ", "address", "22-22-22", "test@mail.ru", "1");
     app.getContactHelper().createContact(contact, true);
     app.getNavigationHelper().returnToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
