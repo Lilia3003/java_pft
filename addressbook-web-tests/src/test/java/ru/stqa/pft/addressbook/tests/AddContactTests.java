10080b3309b2d3b4ed95d8805a3e6a58e5ac6dee
Lilia3003 <73375560+Lilia3003@users.noreply.github.com>
2021-03-13 20:53:42 +0700

    Реализована загрузка тестовых данных из файла для групп/контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,20 +16,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class AddContactTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts(){
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new ContactData().withFirstname("Firstname1").withLastname("Lastname 1").withAddress("Address 1").withEmail("Email1@mail.ru").withGroup("Group1")});
-    list.add(new Object[]{new ContactData().withFirstname("Firstname2").withLastname("Lastname 2").withAddress("Address 2").withEmail("Email2@mail.ru").withGroup("Group2")});
-    list.add(new Object[]{new ContactData().withFirstname("Firstname3").withLastname("Lastname 3").withAddress("Address 3").withEmail("Email3@mail.ru").withGroup("Group3")});
-
-
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
-  @Test (dataProvider = "validContacts")
+  @Test(dataProvider = "validContacts")
   public void testAddContact(ContactData contact) throws Exception {
     Contacts before = app.contact().all();
-    File photo  = new File("src/test/resources/test.png");
+    File photo = new File("src/test/resources/test.png");
     app.contact().create(contact, true);
     app.goTo().HomePage();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
@@ -39,17 +41,17 @@ public class AddContactTests extends TestBase {
 
   }
 
-  @Test (enabled = false)
-  public void testCurrentDir(){
+  @Test(enabled = false)
+  public void testCurrentDir() {
     File currentDir = new File(".");
     System.out.println(currentDir.getAbsolutePath());
-    File photo  = new File("src/test/resources/test.png");
+    File photo = new File("src/test/resources/test.png");
     System.out.println(photo.getAbsolutePath());
     System.out.println(photo.exists());
 
   }
 
-  @Test (enabled = false)
+  @Test(enabled = false)
   public void testBadAddContact() throws Exception {
     Contacts before = app.contact().all();
     ContactData contact = new ContactData().withFirstname("NameХ'").withLastname("LastNameХ").withAddress("address").withPhone("22-22-22").withEmail("test@mail.ru").withGroup("1");
