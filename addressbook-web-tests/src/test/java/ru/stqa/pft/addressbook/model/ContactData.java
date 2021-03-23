a0d55ddf2f5473f0a03feebdca4b9235a1d2abe8
Lilia3003 <73375560+Lilia3003@users.noreply.github.com>
2021-03-23 09:27:01 +0700

    Добавлена возможность извлечения данных из БД для ContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,34 +3,62 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
+
+@Entity
+@Table(name="addressbook")
 
 @XStreamAlias("contact")
 
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name="id")
   private int id = Integer.MAX_VALUE;
   ;
   @Expose
+  @Column(name="firstname")
   private String firstname;
   @Expose
+  @Column(name="lastname")
   private String lastname;
   @Expose
+  @Column(name="address")
+  @Type(type = "text")
   private String address;
-
+  @Transient
   private String phone;
+  @Column(name="email")
+  @Type(type = "text")
   private String email;
+  @Column(name="email2")
+  @Type(type = "text")
   private String email2;
+  @Column(name="email3")
+  @Type(type = "text")
   private String email3;
+  @Transient
   private String allEmails;
+  @Column(name="home")
+  @Type(type = "text")
   private String home;
+  @Type(type = "text")
+  @Column(name="mobile")
   private String mobile;
+  @Type(type = "text")
+  @Column(name="work")
   private String work;
+  @Transient
   private String allPhones;
+  @Transient
   private String group;
-  private File photo;
+  @Column(name="photo")
+  @Type(type = "text")
+  private String photo;
 
   @Override
   public boolean equals(Object o) {
@@ -52,7 +80,7 @@ public class ContactData {
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
@@ -155,7 +183,7 @@ public class ContactData {
 
   public String getAllPhones() { return allPhones; }
 
-  public File getPhoto() { return photo; }
+  public File getPhoto() { return new File(photo); }
 
   @Override
   public String toString() {
